82228c4bdf5b957bb0a9b95a9cade870b9bb05b1
Hasan Deeb <sektories7@gmail.com>
2023-10-26 19:02:27 +0300

    Initial structure commit

---
+++ Frontend/src/App.js
@@ -0,0 +1,29 @@
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+import Footer from './layouts/Footer/Footer';
+import Header from './layouts/Header/Header';
+import Home from './pages/Home/Home';
+import Portfolio from './pages/Portfolio/Portfolio';
+import Services from './pages/Services/Services';
+import AboutUs from './pages/About/AboutUs';
+import ContactUs from './pages/ContactUs/ContactUs';
+
+function App() {
+
+  
+  return (
+    <div className="App">
+      <Header></Header>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/portfolio' element={<Portfolio/>}/>
+        <Route path='/services' element={<Services/>}/>
+        <Route path='/about' element={<AboutUs/>}/>
+        <Route path='/contact' element={<ContactUs/>}/>
+      </Routes>
+      <Footer></Footer>
+    </div>
+  );
+}
+
+export default App;
---
+++ Frontend/src/config/config.js
@@ -0,0 +1,1 @@
+// Store environment variables
