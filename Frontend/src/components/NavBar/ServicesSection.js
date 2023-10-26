530d4a28002766a6796de362ad4f2027310100a3
Hasan Deeb <sektories7@gmail.com>
2023-10-26 21:36:38 +0300

    added assets

---
+++ Frontend/src/components/NavBar/ServicesSection.js
@@ -0,0 +1,9 @@
+
+function ServicesSection() {
+
+  return (
+    <div>ServicesSection</div>
+  )
+}
+
+export default ServicesSection
--- Frontend/src/pages/Home/Home.js
+++ Frontend/src/pages/Home/Home.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import ServicesSection from '../../components/NavBar/ServicesSection'
 
 function Home() {
   return (
-    <div>Home</div>
+    <>
+    <ServicesSection />
+    </>
   )
 }
 
