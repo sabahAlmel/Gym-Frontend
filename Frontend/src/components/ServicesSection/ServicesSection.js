149218fbd8dafd2689b3a9ca2df08b6ba48b9d4d
Hasan Deeb <sektories7@gmail.com>
2023-10-27 19:50:43 +0300

    created the first image section of the services in homepage

--- Frontend/src/components/ServicesSection/ServicesSection.js
+++ Frontend/src/components/ServicesSection/ServicesSection.js
@@ -9,15 +9,18 @@ function ServicesSection() {
     <section className={style.servicesSection}>
       <h3>Services</h3>
       <section className={style.imagesContainer}>
+        <section className={style.heroImageContainer}>
+
         <section className={style.heroImage}>
+        </section>
           <span className={style.heroSectionText}>
-            <h2>Gym</h2>
+            <span className={style.heroTitle}>GYM</span>
             <p>
               Expect a heart-pumping workout that will leave you feeling
               energized and accomplished. Our supportive community of
               like-minded individuals.
             </p>
-          </span>{" "}
+          </span>
         </section>
         <section className={style.serviceImage}></section>
         <section className={style.serviceImage}></section>
