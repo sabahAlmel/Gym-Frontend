60b6e49d791ebe8b3111ed18faf3384ef4020131
Abdulrahman Ghassa <abdulrahman.ghassa93@gmail.com>
2023-10-27 23:51:47 +0300

    Hints for font-size in App.css

--- Frontend/src/pages/Home/Home.js
+++ Frontend/src/pages/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import ServicesSection from '../../components/NavBar/ServicesSection'
+import GymPlans from '../../components/Plans/GymPlans'
 
 function Home() {
   return (
     <>
     <ServicesSection />
+    <GymPlans />
     </>
   )
 }
