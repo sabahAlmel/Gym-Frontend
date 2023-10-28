fa53110df0244a563d811ac955efd6a4445d7135
Hasan Deeb <sektories7@gmail.com>
2023-10-28 20:36:36 +0300

    wrapped the homepage with flex wrapper

--- Frontend/src/pages/Home/Home.js
+++ Frontend/src/pages/Home/Home.js
@@ -2,13 +2,14 @@ import React from 'react'
 
 import ServicesSection from '../../layouts/HomepageServices/index'
 import GymPlans from '../../components/Plans/GymPlans'
+import './home.css'
 
 function Home() {
   return (
-    <> 
+    <section className='homeWrapper'> 
     <ServicesSection />
     <GymPlans />
-    </>
+    </section>
   )
 }
 
