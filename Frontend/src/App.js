abbbdfae0bd70f14abe4cceed643c4feeb53b3a1
Mariamsuleiman <suleimanmariam591@gmail.com>
2023-10-28 18:36:31 +0300

    Footer

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -21,7 +21,7 @@ function App() {
         <Route path='/about' element={<AboutUs/>}/>
         <Route path='/contact' element={<ContactUs/>}/>
       </Routes>
-      <Footer></Footer>
+      <Footer/>
     </div>
   );
 }
