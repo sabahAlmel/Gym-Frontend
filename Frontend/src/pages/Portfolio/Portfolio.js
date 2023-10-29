c235c25b0e09397a17d68b153c4f1631b8913474
Hasan Deeb <sektories7@gmail.com>
2023-10-29 14:57:34 +0200

    added gym Equipment Section to the portfolio page

--- Frontend/src/pages/Portfolio/Portfolio.js
+++ Frontend/src/pages/Portfolio/Portfolio.js
@@ -1,10 +1,12 @@
 import React from "react";
 import HeroSection from "./HeroSection";
+import GymEquipmentSection from "../../layouts/PortfolioEquipment/index.js";
 
 function Portfolio() {
   return (
     <section className="portfolioWrapper">
       <HeroSection />
+      <GymEquipmentSection /> 
     </section>
   );
 }
