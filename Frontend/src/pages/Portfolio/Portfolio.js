3b5002ad707e4b5b36524796879db18f7e807fe3
Hasan Deeb <sektories7@gmail.com>
2023-10-29 15:25:27 +0200

    added Testimonials section to Portfolio page

--- Frontend/src/pages/Portfolio/Portfolio.js
+++ Frontend/src/pages/Portfolio/Portfolio.js
@@ -1,12 +1,14 @@
 import React from "react";
 import HeroSection from "./HeroSection";
 import GymEquipmentSection from "../../layouts/PortfolioEquipment/index.js";
+import PortfolioTestimonials from "../../layouts/PortfolioTestimonials/index";
 
 function Portfolio() {
   return (
     <section className="portfolioWrapper">
       <HeroSection />
       <GymEquipmentSection /> 
+      <PortfolioTestimonials />
     </section>
   );
 }
