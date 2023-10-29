08326d505061e487c5aa962ac582bda4684cedaf
Hasan Deeb <sektories7@gmail.com>
2023-10-29 18:07:17 +0200

    merged newSouhad into master

--- Frontend/src/components/NavBar/Header.js
+++ Frontend/src/components/NavBar/Header.js
@@ -1,11 +1,18 @@
-import React, { useState } from "react";
+import React, { useState,useEffect} from "react";
 import styles from "./Header.module.css";
 import logo from '../../assets/images/logo/logo.svg'
 import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai'
-import { NavLink } from "react-router-dom";
+import { NavLink , useLocation} from "react-router-dom";
 
 const Header = () => {
   const [nav, setnav] = useState(false);
+
+  const location = useLocation();
+
+  useEffect(() => {
+    // Close the mobile menu when a NavLink is clicked
+    setnav(false);
+  }, [location]);
 
 
 
@@ -14,11 +21,11 @@ const Header = () => {
       <img src={logo} alt="/" />
       <nav>
         <ul className={nav ? [styles.menu, styles.active].join(' ') : [styles.menu]}>
-          <li><NavLink exact to='/' activeClassName={styles.activeLink}>Home</NavLink></li>
-          <li><NavLink to='/services' activeClassName={styles.activeLink}>Services</NavLink></li>
-          <li><NavLink to='/portfolio' activeClassName={styles.activeLink}>Portfolio</NavLink></li>
-          <li><NavLink to='/about' activeClassName={styles.activeLink}>About Us</NavLink></li>
-          <li><NavLink to='/contact' activeClassName={styles.activeLink}>Contact Us</NavLink></li>
+          <li><NavLink exact to='/' activeClassName={styles.activeLink} className={`${styles.menuItem} ${location.pathname === '/' ? styles.activeNavItem : ''}`}>Home</NavLink></li>
+          <li><NavLink to='/services' activeClassName={styles.activeLink} className={`${styles.menuItem} ${location.pathname === '/services' ? styles.activeNavItem : ''}`}>Services</NavLink></li>
+          <li><NavLink to='/portfolio' activeClassName={styles.activeLink} className={`${styles.menuItem} ${location.pathname === '/portfolio' ? styles.activeNavItem : ''}`}>Portfolio</NavLink></li>
+          <li><NavLink to='/about' activeClassName={styles.activeLink} className={`${styles.menuItem} ${location.pathname === '/about' ? styles.activeNavItem : ''}`}>About Us</NavLink></li>
+          <li><NavLink to='/contact' activeClassName={styles.activeLink} className={`${styles.menuItem} ${location.pathname === '/contact' ? styles.activeNavItem : ''}`}>Contact Us</NavLink></li>
         </ul>
 
       </nav>
--- Frontend/src/pages/Home/Home.js
+++ Frontend/src/pages/Home/Home.js
@@ -2,10 +2,12 @@ import React from 'react'
 
 import ServicesSection from '../../layouts/HomepageServices/index'
 import GymPlans from '../../components/Plans/GymPlans'
+import ReasonsToJoin from '../../components/ReasonsToJoin/ReasonsToJoin'
 
 function Home() {
   return (
     <section className='homeWrapper'> 
+    <ReasonsToJoin />
     <ServicesSection />
     <GymPlans />
     </section>
