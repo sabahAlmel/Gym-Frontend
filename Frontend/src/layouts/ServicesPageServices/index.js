f074fe402ce167c9a4ec0eb9ca908ac0644e13b8
Hasan Deeb <sektories7@gmail.com>
2023-10-30 16:09:42 +0200

    added personal training section

--- Frontend/src/layouts/ServicesPageServices/index.js
+++ Frontend/src/layouts/ServicesPageServices/index.js
@@ -1,7 +1,9 @@
 import React from "react";
-import stretchingImg from '../../assets/images/ServicesImages/Stretching.png'
-import coreImg from '../../assets/images/ServicesImages/Core.png'
-import boxingImg from '../../assets/images/ServicesImages/Boxing.png'
+import stretchingImg from "../../assets/images/ServicesImages/Stretching.png";
+import coreImg from "../../assets/images/ServicesImages/Core.png";
+import boxingImg from "../../assets/images/ServicesImages/Boxing.png";
+import PersonalTraining from "../../components/PersonalTraining/PersonalTraining";
+import style from "./ServicesPageServices.module.css";
 
 export default function ServicesPageServices() {
   const fakeData = [
@@ -9,25 +11,41 @@ export default function ServicesPageServices() {
       name: "Core",
       description:
         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum sed, libero eos ipsa qui itaque possimus praesentium reiciendis, excepturi exercitationem asperiores aliquam nostrum voluptate pariatur consectetur architecto. Molestias, et maiores!",
-        image: coreImg
+      image: coreImg,
     },
     {
       name: "Boxing",
       description:
         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum sed, libero eos ipsa qui itaque possimus praesentium reiciendis, excepturi exercitationem asperiores aliquam nostrum voluptate pariatur consectetur architecto. Molestias, et maiores!",
-        image: boxingImg
+      image: boxingImg,
     },
     {
       name: "Stretching",
       description:
         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum sed, libero eos ipsa qui itaque possimus praesentium reiciendis, excepturi exercitationem asperiores aliquam nostrum voluptate pariatur consectetur architecto. Molestias, et maiores!",
-        image: stretchingImg
+      image: stretchingImg,
     },
-    
   ];
   return (
-    fakeData.map(item =>{
-        <PersonalTraining name={item.name} description={item.description} image={item.image}/>
-    })
-  )
+    <section className={style.personalTrainingContainer}>
+        <section className={style.personalTrainingHeader}>
+      <h2>Services</h2>
+      <p>
+        Transform your potential into power with our training services and
+        redefine what your body is capable of.
+      </p>
+        </section>
+      <section className={style.personalTrainingFlex}>
+
+      {fakeData.map((item, key) => (
+          <PersonalTraining
+          key={key}
+          name={item.name}
+          description={item.description}
+          image={item.image}
+          />
+          ))}
+          </section>
+    </section>
+  );
 }
--- Frontend/src/pages/Services/Services.js
+++ Frontend/src/pages/Services/Services.js
@@ -1,19 +1,25 @@
-import React from 'react'
+import React from "react";
 
-import servicesHero from '../../assets/images/ServicesImages/servicesHero.png'
-import Products from '../../layouts/Products/Products'
-import style from './Services.module.css'
-
+import servicesHero from "../../assets/images/ServicesImages/servicesHero.png";
+import Products from "../../layouts/Products/Products";
+import style from "./Services.module.css";
+import ServicesPageServices from "../../layouts/ServicesPageServices/index";
 
 function Services() {
   return (
     <>
-    <section className={style.servicesHeroImage}><span className={style.servicesHeroTextContainer}><span style={{color: 'var(--red-text-clr)'}}>Make</span> Your Dream Body</span></section>
-    <section className='servicesWrapper'>
-    <Products />
-    </section>
+      <section className={style.servicesHeroImage}>
+        <span className={style.servicesHeroTextContainer}>
+          <span style={{ color: "var(--red-text-clr)" }}>Make</span> Your Dream
+          Body
+        </span>
+      </section>
+      <section className="servicesWrapper">
+        <Products />
+        <ServicesPageServices />
+      </section>
     </>
-  )
+  );
 }
 
-export default Services
+export default Services;
