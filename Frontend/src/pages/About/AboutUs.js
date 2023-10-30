20b527fe0183226c59e5c5800ef83e077823acf7
Abdulrahman Ghassa <abdulrahman.ghassa93@gmail.com>
2023-10-30 15:34:11 +0200

    Our Value Correction

--- Frontend/src/pages/About/AboutUs.js
+++ Frontend/src/pages/About/AboutUs.js
@@ -1,6 +1,6 @@
 
 import React from 'react'
-import OurValue from './OurValue'
+import OurValue from '../../components/OurValue/OurValue'
 import Hero from '../../components/HeroAboutUs/HeroAbouUs'; // Import the Hero component
 import OurStory from '../../components/OurStory/OurStory';
 import OurCulture  from '../../components/NavBar/OurCulture/OurCulture';
