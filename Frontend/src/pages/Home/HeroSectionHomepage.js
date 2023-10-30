da560847d957ab1f9908af8d9fb932fed2a872fd
Mariamsuleiman <suleimanmariam591@gmail.com>
2023-10-30 12:00:14 +0200

    hompage

--- Frontend/src/pages/Home/HeroSectionHomepage.js
+++ Frontend/src/pages/Home/HeroSectionHomepage.js
@@ -4,7 +4,12 @@ import HeroSectionHomepageCss from"./HeroSectionHomapage.module.css"
 const HeroSectionHomepage = () => {
     return(
         <div className={HeroSectionHomepageCss.containerHero}>
-            <div className={HeroSectionHomepageCss.backgroundImage}></div>
+            <div className={HeroSectionHomepageCss.backgroundImage}>
+                <div className={HeroSectionHomepageCss.content}>
+                    <h1 className={HeroSectionHomepageCss.tite}>work<span className={HeroSectionHomepageCss.span}>harder</span>get stronger</h1>
+                    <button className={HeroSectionHomepageCss.button}>join us</button>
+                </div>   
+            </div>
         </div>
     )
 }
