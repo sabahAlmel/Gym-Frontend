815b1a476a9470495791a85cc862e2f9f0a4c750
Hasan Deeb <sektories7@gmail.com>
2023-10-31 21:52:19 +0200

    sideBar modification

--- Frontend/src/pages/Dashboard/dashboard.js
+++ Frontend/src/pages/Dashboard/dashboard.js
@@ -1,18 +1,57 @@
-import React from 'react'
-import style from './Dashboard.module.css'
-import dash from '../../assets/icons/dashLogo.svg'
-import contentLogo from '../../assets/icons/ContentLogo.svg'
+import React, { useState } from "react";
+import style from "./Dashboard.module.css";
+import dash from "../../assets/icons/dashLogo.svg";
+import contentLogo from "../../assets/icons/ContentLogo.svg";
+import { NavLink } from "react-router-dom";
 function Dashboard(props) {
-    const {setIsOnDashboard} = props
-    setIsOnDashboard(true)
+  const { setIsOnDashboard } = props;
+  setIsOnDashboard(true);
+
+  const [isSideItem, setIsSideItem] = useState(false);
+  function toggleItems() {
+    setIsSideItem(!isSideItem);
+  }
+
   return (
     <div className={style.dashboardWrapper}>
-        <section className={style.dashboardSideNav}>
-      <span className={style.dashLogoContainer}><img src={dash} alt="Dashboard Logo" />Dashboard</span>
-            <a href="#"><img src={contentLogo} alt="Content management icon" /> Content Management</a>
+      <section className={style.dashboardSideNav}>
+        <span className={style.dashLogoContainer}>
+          <img src={dash} alt="Dashboard Logo" />
+          Dashboard
+        </span>
+        <section className={style.navItemsWrapper}>
+          <a href="#" className={style.sideBarItem} onClick={toggleItems}>
+            <img src={contentLogo} alt="Content management icon" /> Content
+            Management
+          </a>
+
+          <ul
+            className={style.ContentManagementItemsWrapper}
+            style={{ height: isSideItem ? "105px" : "0" }}
+          >
+            <li className={`${style.sideBarItem} ${style.sideBarSubItem}`}>
+              <NavLink
+                exact
+                className={style.sideBarSubItem}
+                activeClassName={style.activeSideBar}
+              >
+                Services
+              </NavLink>
+            </li>
+            <li className={`${style.sideBarItem} ${style.sideBarSubItem}`}>
+              <NavLink
+                exact
+                className={style.sideBarSubItem}
+                activeClassName={style.activeSideItem}
+              >
+                Products
+              </NavLink>
+            </li>
+          </ul>
         </section>
+      </section>
     </div>
-  )
+  );
 }
 
-export default Dashboard
+export default Dashboard;
