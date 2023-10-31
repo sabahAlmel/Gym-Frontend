cbca7922c1c748badbe2da386f4ba59bca622dee
Mariamsuleiman <suleimanmariam591@gmail.com>
2023-10-31 02:03:04 +0200

    regime

--- Frontend/src/pages/Services/RegimeService.js
+++ Frontend/src/pages/Services/RegimeService.js
@@ -1,32 +1,43 @@
 import React from "react";
 import style from "./RegimeServices.module.css";
-import image1 from "../../assets/images/ServicesImages/Rectangle 1.png"
-import image2 from "../../assets/images/ServicesImages/Rectangle 1-1.png"
-import image3 from "../../assets/images/ServicesImages/Rectangle 1-2.png"
+import image1 from "../../assets/images/ServicesImages/Rectangle 1.png";
+import image2 from "../../assets/images/ServicesImages/Rectangle 1-1.png";
+import image3 from "../../assets/images/ServicesImages/Rectangle 1-2.png";
 
-const RegimeServices = () =>{
-    return(
-        <div className={style.container}>
-            <h3 className={style.topic}>Every Meals Counts: A Complete Body-Type Nutrition Guide!</h3>
-            <div className={style.content}>
-            <div className={style.imgs}>
-                <img src={image1} alt='food' className={style.img}></img>
-                <img  src={image2} alt ='soup' className={style.img}></img>
-                <img src={image3} alt= 'fruits' className={style.img}></img>
-            </div>
-            <div className={style.titles}>
-                <h4 className={style.title}> Expert Guidance</h4>
-                <h4 className={style.title}>Ongoing Support</h4>
-                <h4 className={style.title}>Real Results</h4>
-            </div>
-            <div className={style.texts}>
-              <p className={style.text}>You'll receive one-on-one guidance from certified trainers who will create and adjust your plan to maximize your results</p>
-              <p className={style.text}> Your journey is ongoing, and so is our support. We're here to help you overcome plateaus and achieve your goals.</p>
-              <p className={style.text}>Our clients have achieved remarkable results, and you can see their transformations in our success stories.</p>
-            </div>
-            </div>
+const RegimeServices = () => {
+  return (
+    <div className={style.container}>
+      <h3 className={style.topic}>
+        Every Meals Counts: A Complete Body-Type Nutrition Guide!
+      </h3>
+      <div className={style.content}>
+        <div className={style.part}>
+          <img src={image1} alt="food" className={style.img}></img>
+          <h4 className={style.title1}>Expert Guidance</h4>
+          <p className={style.text}>
+            You'll receive one-on-one guidance from certified trainers who will
+            create and adjust your plan to maximize your results
+          </p>
         </div>
-    )
-}
+        <div className={style.part}>
+          <img src={image2} alt="soup" className={style.img}></img>
+          <h4 className={style.title2}>Ongoing Support</h4>
+          <p className={style.text}>
+            Your journey is ongoing, and so is our support. We're here to help
+            you overcome plateaus and achieve your goals.
+          </p>
+        </div>
+        <div className={style.part}>
+          <img src={image3} alt="fruits" className={style.img}></img>
+          <h4 className={style.title3}>Real Results</h4>
+          <p className={style.text}>
+            Our clients have achieved remarkable results, and you can see their
+            transformations in our success stories.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default RegimeServices;
