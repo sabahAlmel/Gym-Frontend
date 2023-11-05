63bda51fc8fc7e919021edd5f88e270aecf23a01
Mariamsuleiman <suleimanmariam591@gmail.com>
2023-11-05 17:08:33 +0200

    regimePlan

--- Frontend/src/components/Plans/GymPlan.js
+++ Frontend/src/components/Plans/GymPlan.js
@@ -1,30 +1,34 @@
-import React from 'react'
-import style from './GymPlan.module.css'
-import CheckIcon from '../../assets/images/HomepageImages/checkIcon.png'
+import React, { useState } from "react";
+import style from "./GymPlan.module.css";
+import CheckIcon from "../../assets/images/HomepageImages/checkIcon.png";
+
+
+
 
 const GymPlan = ({ title, price, plans }) => {
-
   return (
     <>
       <div className={style.gymPlanCard}>
         <h3 className={style.gymPlanType}>{title}</h3>
         <p className={style.membership}>Membership</p>
-        <h3 className={style.gymPlanPrice}>$<span className={style.price}>{price}</span> Per Month</h3>
+        <h3 className={style.gymPlanPrice}>
+          $<span className={style.price}>{price}</span> Per Month
+        </h3>
         <div className={style.check}>
           {plans.map((plan, i) => (
             <div className={style.descriptionArray} key={i}>
-              <img className={style.checkIcon} src={CheckIcon} alt='check' />
+              <img className={style.checkIcon} src={CheckIcon} alt="check" />
               <p className={style.descriptionText}>{plan.feature[i]}</p>
             </div>
           ))}
         </div>
         <div className={style.buttonSubscribe}>
-          <button className={style.gymPlanButton}>Subscribe</button>  
+          <button  className={style.gymPlanButton}>
+            Subscribe
+          </button>
         </div>
       </div>
     </>
-  )
-}
-
-
-export default GymPlan
+  );
+};
+export default GymPlan;
