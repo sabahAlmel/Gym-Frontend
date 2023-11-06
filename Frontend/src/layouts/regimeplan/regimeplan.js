a6c373fbfddec59fca502b49ce1557299769072d
Mariamsuleiman <suleimanmariam591@gmail.com>
2023-11-06 09:12:44 +0200

    regime

--- Frontend/src/layouts/regimeplan/regimeplan.js
+++ Frontend/src/layouts/regimeplan/regimeplan.js
@@ -1,37 +1,35 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import styles from "./regimeplan.module.css";
-import image1 from "../../assets/images/ServicesImages/Rectangle 1.png";
-import image2 from "../../assets/images/ServicesImages/Rectangle 1-1.png";
-import image3 from "../../assets/images/ServicesImages/Rectangle 1-2.png";
+// import image1 from "../../assets/images/ServicesImages/Rectangle 1.png";
+// import image2 from "../../assets/images/ServicesImages/Rectangle 1-1.png";
+// import image3 from "../../assets/images/ServicesImages/Rectangle 1-2.png";
+import { fetchRegime } from "../../db/regimeData";
 import Regimeplancomponent from "../../components/regimeplan/regimeplansection";
 
-
-
-export default function Regimeplansection() {
-    const fakeData = [
-        {
-            name: "Expert Guidance",
-            description:"You'll receive one-on-one guidance from certified trainers who will create and adjust your plan to maximize your results",
-            image: image1,
-        },
-        {
-            name: "Ongoing Support",
-            description: " Your journey is ongoing, and so is our support. We're here to help you overcome plateaus and achieve your goals.",
-            image: image2,
-        },
-        {
-            name: "Real Results",
-            description: " Our clients have achieved remarkable results, and you can see their transformations in our success stories.",
-            image: image3,
-        },
-    ];
+const RegimePlans = ()=> { 
+    const [regimePlans , setRegimePlans] = useState([]);
+    async function fetchData(){
+        try{
+            const data = await fetchRegime()
+        if (data){
+            setRegimePlans(data.data);
+        }else{
+        }
+        }catch(error){
+            console.log(error)
+        }
+    }
+    useEffect( ()=>{
+        fetchData()
+    },[]);
     return (
         <section className={styles.RegimePlanContainer}>
             <section className={styles.RegimePlanHeader}>
                 <h2> Every Meals Counts: A Complete Body-Type Nutrition Guide!</h2>
             </section>
             <section className={styles.RegimeplanFlex}>
-                {fakeData.map((item, key) => (
+                {
+                regimePlans.map((item, key) => (
                     <Regimeplancomponent
                         key={key}
                         name={item.name}
@@ -43,3 +41,4 @@ export default function Regimeplansection() {
         </section>
     );
 }
+export default RegimePlans;
